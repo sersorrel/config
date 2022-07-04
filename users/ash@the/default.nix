8a68bdba25efedd18c10f055e93f98e44802577c
ash <ash@sorrel.sh>
2022-07-04 04:37:35 +0100

    Add thumbnailer for XCF files

--- users/ash@the/default.nix
+++ users/ash@the/default.nix
@@ -16,6 +16,15 @@
 
     programs.home-manager.enable = true;
 
+    # https://moritzmolch.com/blog/1749.html, https://docs.xfce.org/xfce/tumbler/available_plugins
+    # note: apparently xcftools is unmaintained since 2019 and nixpkgs' version has multiple code-execution vulnerabilities, so let's not use xcf2png
+    xdg.dataFile."thumbnailers/xcf.thumbnailer".text = ''
+      [Thumbnailer Entry]
+      TryExec=${pkgs.imagemagick}/bin/convert
+      Exec=${pkgs.imagemagick}/bin/convert xcf:%i -flatten -scale 512x%s png:%o
+      MimeType=image/x-xcf;
+    '';
+
     systemd.user.targets = {
       # Workaround for "Unit tray.target not found" when starting e.g. flameshot
       # https://github.com/nix-community/home-manager/issues/2064
