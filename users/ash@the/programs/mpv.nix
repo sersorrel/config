eb4e994733ea8bea280ca358b78322999f0c31a4
ash <ash@sorrel.sh>
2022-07-04 04:30:55 +0100

    Show mpv OSC permanently when only playing audio

--- users/ash@the/programs/mpv.nix
+++ users/ash@the/programs/mpv.nix
@@ -15,6 +15,21 @@
     };
     scripts = [
       pkgs.mpvScripts.mpris
+      ((pkgs.writeTextFile {
+        name = "mpv-audio-osc";
+        executable = true;
+        destination = "/share/mpv/scripts/audio-osc.lua";
+        text = ''
+          mp.register_event("file-loaded", function()
+            local hasvid = mp.get_property_osd("video") ~= "no"
+            mp.commandv("script-message", "osc-visibility", (hasvid and "auto" or "always"), "no-osd")
+            -- remove the next line if you don't want to affect the osd-bar config
+            mp.commandv("set", "options/osd-bar", (hasvid and "yes" or "no"))
+          end)
+        '';
+      }).overrideAttrs (old: {
+        passthru.scriptName = "audio-osc.lua";
+      }))
     ];
     # TODO: mute on step forwards: https://github.com/mpv-player/mpv/issues/6104
   };
