5ecaaa5b136a51dbf66934dac2a0ee6a199784cd
ash <ash@sorrel.sh>
2022-07-11 22:30:52 +0100

    Make mpv OSC show on audio files even when embedded album art is present

--- users/ash@the/programs/mpv.nix
+++ users/ash@the/programs/mpv.nix
@@ -20,8 +20,15 @@
         executable = true;
         destination = "/share/mpv/scripts/audio-osc.lua";
         text = ''
+          function ends_with(value, suffix)
+            return suffix == "" or value:sub(-#suffix) == suffix
+          end
           mp.register_event("file-loaded", function()
             local hasvid = mp.get_property_osd("video") ~= "no"
+            -- that's not accurate if there's embedded album art, so we also test the filename
+            local path = mp.get_property("path")
+            hasvid = hasvid and not ends_with(path, ".flac")
+            hasvid = hasvid and not ends_with(path, ".mp3")
             mp.commandv("script-message", "osc-visibility", (hasvid and "auto" or "always"), "no-osd")
             -- remove the next line if you don't want to affect the osd-bar config
             mp.commandv("set", "options/osd-bar", (hasvid and "yes" or "no"))
