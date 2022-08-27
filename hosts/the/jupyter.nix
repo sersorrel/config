06c1f6f81812f674efc59cfd2f67732f1c2a88c4
ash <ash@sorrel.sh>
2022-08-27 16:47:58 +0100

    Stop explicitly marking jupyter as a system user
    This is now handled by the module.

--- hosts/the/jupyter.nix
+++ hosts/the/jupyter.nix
@@ -2,5 +2,4 @@
   services.jupyter.enable = true;
   services.jupyter.password = "''"; # use auth tokens rather than passwords
   users.users.jupyter.group = "jupyter";
-  users.users.jupyter.isSystemUser = true;
 }
