b5785565788fadb4dc21aa381cafe78e0be8e63d
ash <ash@sorrel.sh>
2022-09-21 15:34:18 +0100

    Add GDB config

--- users/ash@the/programs/gdb.nix
+++ users/ash@the/programs/gdb.nix
@@ -5,4 +5,9 @@
   xdg.configFile."gdb/gdbearlyinit".text = ''
     set startup-quietly on
   '';
+  xdg.configFile."gdb/gdbinit".text = ''
+    set print thread-events off
+    set pagination off
+    set non-stop on
+  '';
 }
