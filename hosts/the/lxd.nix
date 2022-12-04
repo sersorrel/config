3d66329221e03a9e94669ba4ef29d1397b1f4f90
ash <ash@sorrel.sh>
2022-12-04 19:40:16 +0000

    Add workaround for LXD module bug

--- hosts/the/lxd.nix
+++ hosts/the/lxd.nix
@@ -1,5 +1,8 @@
+{ pkgs, ... }:
+
 {
   virtualisation.lxd.enable = true;
   virtualisation.lxc.lxcfs.enable = true;
   # TODO: maybe consider virtualisation.lxd.recommendedSysctlSettings: https://linuxcontainers.org/lxd/docs/master/production-setup/
+  systemd.services.lxd.path = [ pkgs.util-linux ]; # TODO: remove once https://github.com/NixOS/nixpkgs/pull/204297 is merged
 }
