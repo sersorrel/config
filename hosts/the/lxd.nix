ba7b6249162313b23b78769023b66ac4f7522ba0
ash <ash@sorrel.sh>
2022-12-06 17:43:08 +0000

    Update inputs

--- hosts/the/lxd.nix
+++ hosts/the/lxd.nix
@@ -4,5 +4,4 @@
   virtualisation.lxd.enable = true;
   virtualisation.lxc.lxcfs.enable = true;
   # TODO: maybe consider virtualisation.lxd.recommendedSysctlSettings: https://linuxcontainers.org/lxd/docs/master/production-setup/
-  systemd.services.lxd.path = [ pkgs.util-linux ]; # TODO: remove once https://github.com/NixOS/nixpkgs/pull/204297 is merged
 }
